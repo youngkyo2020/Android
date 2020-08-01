aba094685b5b4744599d0b316da0068e83770944
youngkyo2020 <youngkyo2020@gmail.com>
2020-08-01 14:10:05 +0900

    "scale anim"

--- app/src/main/java/com/example/chapter7recycler/MainActivity.java
+++ app/src/main/java/com/example/chapter7recycler/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.chapter7recycler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation anim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.scale);
+                view.startAnimation(anim);
+            }
+        });
     }
 }
